60e414ffd7415b3ab6eff095c01bd6df4c6db760
Joseph Kim <jkjoeeekim@live.com>
2021-05-14 14:52:08 -0700

    Add exercises for project 2

---
+++ meddling_medley/meddling_medley_part1.rb
@@ -0,0 +1,88 @@
+def duos(str)
+
+end
+
+p duos('bootcamp')      # 1
+p duos('wxxyzz')        # 2
+p duos('hoooraay')      # 3
+p duos('dinosaurs')     # 0
+p duos('e')             # 0
+
+
+def sentence_swap(sentence, hash)
+
+end
+
+p sentence_swap('anything you can do I can do',
+    'anything'=>'nothing', 'do'=>'drink', 'can'=>'shall'
+) # 'nothing you shall drink I shall drink'
+
+p sentence_swap('what a sad ad',
+    'cat'=>'dog', 'sad'=>'happy', 'what'=>'make'
+) # 'make a happy ad'
+
+p sentence_swap('keep coding okay',
+    'coding'=>'running', 'kay'=>'pen'
+) # 'keep running okay'
+
+
+def hash_mapped(hash, prc1, &prc2)
+
+end
+
+double = Proc.new { |n| n * 2 }
+p hash_mapped({'a'=>4, 'x'=>7, 'c'=>-3}, double) { |k| k.upcase + '!!' }
+# {"A!!"=>8, "X!!"=>14, "C!!"=>-6}
+
+first = Proc.new { |a| a[0] }
+p hash_mapped({-5=>['q', 'r', 's'], 6=>['w', 'x']}, first) { |n| n * n }
+# {25=>"q", 36=>"w"}
+
+
+def counted_characters(str)
+
+end
+
+p counted_characters("that's alright folks") # ["t"]
+p counted_characters("mississippi") # ["i", "s"]
+p counted_characters("hot potato soup please") # ["o", "t", " ", "p"]
+p counted_characters("runtime") # []
+
+
+def triplet_true?(str)
+
+end
+
+p triplet_true('caaabb')        # true
+p triplet_true('terrrrrible')   # true
+p triplet_true('runninggg')     # true
+p triplet_true('bootcamp')      # false
+p triplet_true('e')             # false
+
+
+def energetic_encoding(str, hash)
+
+end
+
+p energetic_encoding('sent sea',
+    'e'=>'i', 's'=>'z', 'n'=>'m', 't'=>'p', 'a'=>'u'
+) # 'zimp ziu'
+
+p energetic_encoding('cat',
+    'a'=>'o', 'c'=>'k'
+) # 'ko?'
+
+p energetic_encoding('hello world',
+    'o'=>'i', 'l'=>'r', 'e'=>'a'
+) # '?arri ?i?r?'
+
+p energetic_encoding('bike', {}) # '????'
+
+
+def uncompress(str)
+
+end
+
+uncompress('a2b4c1') # 'aabbbbc'
+uncompress('b1o2t1') # 'boot'
+uncompress('x3y1x2z4') # 'xxxyxxzzzz'
